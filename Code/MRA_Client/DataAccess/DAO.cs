56b7d7cab4226083cd8d5ddf71be93622a679fdc
linhlthe150526 <linhlthe150526@fpt.edu.vn>
2022-07-25 22:35:29 +0700

    up project

---
+++ Code/MRA_Client/DataAccess/DAO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA_Client.DataAccess {
+    class DAO {
+
+
+
+        private static SqlConnection GetConnection() {
+            string ConnectionString = "server=LAPTOP-DV5UMJFC;database=MRA_Project;uid=sa;pwd=linhkidno1";
+            return new SqlConnection(ConnectionString);
+        }
+
+
+
+        public static DataTable GetDataBySQL(string sql, SqlParameter[] parameters) {
+            using (SqlCommand command = new SqlCommand(sql, GetConnection())) {
+                command.Parameters.AddRange(parameters);
+                command.Connection.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                DataSet dataSet = new DataSet();
+
+                da.Fill(dataSet);
+
+                command.Connection.Close();
+                return dataSet.Tables[0];
+            }
+        }
+
+
+
+        public static int ExecuteSQL(string sql, SqlParameter[] parameters) {
+            using (SqlCommand command = new SqlCommand(sql, GetConnection())) {
+                command.Parameters.AddRange(parameters);
+                command.Connection.Open();
+                int count = command.ExecuteNonQuery();
+                command.Connection.Close();
+                return count;
+            }
+        }
+    }
+}
